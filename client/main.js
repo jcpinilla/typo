13d563d068999832d160c3df8c44a606a0e324bb
Juan Camilo Pinilla <jc.pinilla@uniandes.edu.co>
2018-04-01 01:13:41 -0500

    css file added to public

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import { Meteor } from "meteor/meteor";
 import ReactDOM from "react-dom";
 
 import "../imports/startup/accounts-config.js";
+import "../imports/startup/load-fonts.js";
 
 import App from "../imports/ui/App.js";
 
---
+++ imports/startup/load-fonts.js
@@ -0,0 +1,10 @@
+import WebFont from "webfontloader";
+
+WebFont.load({
+	google: {
+		families: [
+			"Anonymous Pro",
+			"Poppins",
+		],
+	},
+});

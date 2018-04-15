1fe0db5e10e3bd88c0e6ff9600f0572db96dd253
Juan Camilo Pinilla <jc.pinilla@uniandes.edu.co>
2018-04-15 05:37:23 -0500

    new test packages

--- imports/ui/Random.js
+++ imports/ui/Random.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Meteor } from "meteor/meteor";
 
 import classnames from "classnames";
 
@@ -8,6 +9,17 @@ export default class Random extends Component {
 		this.state = {
 			waiting: false
 		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		let waiting = this.state.waiting;
+		if (!waiting) {
+			Meteor.call("games.insert");
+			this.setState({
+				waiting: !waiting
+			});
+		}
 	}
 
 	render() {
@@ -23,19 +35,21 @@ export default class Random extends Component {
 		);
 		let spinner = null;
 		if (waiting) {
-			spinner = <i className="fa fa-spinner fa-spin"></i>;
+			spinner = <i id="waiting-spinner" className="fa fa-spinner fa-spin"></i>;
 		}
 		let button = (
 			<button
 				type="button"
-				className="btn btn-primary">
+				className="btn btn-primary"
+				onClick={this.handleClick}>
 				<i className={className}></i>
 			</button>
 		);
 		return (
 			<div>
-				<h1>{title}</h1>
+				<h2 id="waiting-title">{title}</h2>
 				{button}
+				<br />
 				{spinner}
 			</div>
 		);
